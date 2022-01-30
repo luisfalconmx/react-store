797ce5f0013a99ff5116b6a33e38282a397d2c90
luisfalconmx <luisfalconmx@gmail.com>
2022-01-29 19:01:53 -0600

    feat: the Footer component was created with styles for darkmode and lightmode

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Image from '@components/Image'
+import Logo from '@images/luisfalconmx-logo.png'
+import LogoWebp from '@images/luisfalconmx-logo.png?as=webp'
+import { GitHub, Facebook, Instagram, Linkedin } from 'react-feather'
+import './index.pcss'
+
+const Footer = () => {
+  const GITHUB_PROFILE = 'https://github.com/luisfalconmx'
+
+  const SocialIcons = [
+    {
+      icon: <GitHub />,
+      url: 'https://github.com/luisfalconmx/spa-store'
+    },
+    {
+      icon: <Facebook />,
+      url: 'https://www.facebook.com/luisfalconmx'
+    },
+    {
+      icon: <Instagram />,
+      url: 'https://www.instagram.com/luisfalconmx'
+    },
+    {
+      icon: <Linkedin />,
+      url: 'https://www.linkedin.com/in/luisfalconmx'
+    }
+  ]
+
+  return (
+    <footer className="Footer">
+      <Link className="Footer__Identity" to="/">
+        <Image
+          className="Footer__Logo"
+          src={Logo}
+          srcSet={LogoWebp}
+          alt="luisfalconmx logo"
+        />
+        <span className="Footer__Title">SPA Store</span>
+      </Link>
+      <div className="Footer__Description">
+        Single Page Application with React, React Router, PostCSS, Webpack,
+        Docker and Docker Compose.
+      </div>
+      <div className="Footer__Leyend">
+        Creado con ❤️ por <a href={GITHUB_PROFILE}>luisfalconmx</a>
+      </div>
+      <div className="Footer__Social">
+        {SocialIcons.map(({ icon, url }) => (
+          <a
+            key={url}
+            href={url}
+            className="Footer__SocialIcon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {icon}
+          </a>
+        ))}
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/containers/Layout/index.jsx
+++ src/containers/Layout/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
-import Navbar from '@containers/Navbar'
-import Footer from '@containers/Footer'
+import Navbar from '@components/Navbar'
+import Footer from '@components/Footer'
 import './index.pcss'
 
 const Layout = ({ children }) => (
