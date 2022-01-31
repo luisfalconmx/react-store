a6b6f7a4f4862f9a028bc2c13e70fc0fb0814137
luisfalconmx <luisfalconmx@gmail.com>
2022-01-31 03:02:32 -0600

    chore: enable the api endpoints

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -4,11 +4,11 @@ import axios from 'axios'
 const useGetProducts = () => {
   const [products, setProducts] = useState([])
 
-  // const API = 'https://my.api.mockaroo.com/products'
-  // const API_KEY = '253a0410'
+  const API = 'https://my.api.mockaroo.com/products'
+  const API_KEY = '253a0410'
 
-  const API = 'http://localhost:4000/products'
-  const API_KEY = '58239'
+  // const API = 'http://localhost:4000/products'
+  // const API_KEY = '58239'
 
   useEffect(async () => {
     const axiosOptions = {
